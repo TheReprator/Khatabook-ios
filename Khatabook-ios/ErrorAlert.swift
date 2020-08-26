759e46b8ff03f8fa004568c2f684dbca39080657
Reprator <mr.vikram004@gmail.com>
2020-08-27 05:05:56 +0530

    design and validation of phone login done

---
+++ Khatabook-ios/ErrorAlert.swift
@@ -0,0 +1,62 @@
+//
+//  ErrorAlert.swift
+//  Khatabook-ios
+//
+//  Created by Vikram Singh on 27/08/20.
+//  Copyright © 2020 Vikram Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ErrorAlert : View {
+    
+    @State var color = Color.black.opacity(0.7)
+    @Binding var alert : Bool
+    @Binding var error : String
+    
+    var body: some View{
+        
+        GeometryReader{_ in
+            
+            VStack{
+                HStack{
+                    Text("Validation Error")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(self.color)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 25)
+                
+                Text(self.error)
+                    .foregroundColor(self.color)
+                    .padding(.top)
+                    .padding(.horizontal, 25)
+                
+                Button(action: {
+                    
+                    self.alert.toggle()
+                    
+                }) {
+                    Text("OK")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(.infinity)
+                        
+                        .frame(width: UIScreen.main.bounds.width - 120)
+                }
+                .background(Color("Color"))
+                .cornerRadius(10)
+                .padding(.top, 25)
+                
+            }
+            .padding(.vertical, 25)
+            .frame(width: UIScreen.main.bounds.width - 70)
+            .background(Color.white)
+            .cornerRadius(15)
+        }
+        .background(Color.black.opacity(0.35).edgesIgnoringSafeArea(.all))
+    }
+}
--- Khatabook-ios/LoginController.swift
+++ Khatabook-ios/LoginController.swift
@@ -9,15 +9,68 @@
 import SwiftUI
 
 struct LoginController: View {
-    @State private var name: String = "Tim"
-
+    
+    @State private var phoneNumber = ""
+    @State private var validationErrorMessage = ""
+    @State private var showAlert = false
+    
     var body: some View {
-        VStack {
-            TextField("Enter your name", text: $name)
-            Text("Hello, \(name)!")
+        ZStack{
+            Color(red: 0.10, green: 0.74, blue: 0.61)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                
+                TextField("9041866055", text: $phoneNumber)
+                    .keyboardType(.phonePad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .foregroundColor(.black)
+                    .padding()
+                
+                Button(action: {
+                    self.validatePhoneNumber()
+                }) {
+                    Text("Submit")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(.infinity)
+                    
+                }
+                .shadow(color: Color.blue, radius: 20, y: 5)
+            }
+            
+            if self.showAlert {
+                ErrorAlert(alert: self.$showAlert, error: self.$validationErrorMessage)
+            }
         }
+        
     }
     
+    
+    private func validatePhoneNumber(){
+        
+        if  phoneNumber.isEmpty {
+            self.validationErrorMessage = "Please enter phone number"
+            self.showAlert = true
+            return
+        }
+        
+        if(phoneNumber.count < 10){
+            self.validationErrorMessage = "Phone number can't be less than 10"
+            self.showAlert = true
+            
+            return
+        }
+        
+        if(phoneNumber.count > 10){
+            self.validationErrorMessage = "Phone number can't be greater than 10"
+            self.showAlert = true
+            
+            return
+        }
+        
+        //Hit api
+    }
 }
 
 struct LoginController_Previews: PreviewProvider {
