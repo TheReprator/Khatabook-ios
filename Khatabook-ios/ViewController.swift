fff340ad42ae81c8c3770740c4aac3d2c011981c
Reprator <mr.vikram004@gmail.com>
2020-08-26 23:43:01 +0530

    login page design with validation complete

--- Khatabook-ios/ViewController.swift
+++ Khatabook-ios/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func submitPhoneNumber(_ sender: Any) {
+        
+        let phoneNumber = phoneTextField.text
+    
+        let validationCallBack = {
+           
+        }
+        
+        if phoneNumber == nil || phoneNumber!.isEmpty {
+            showAlertDialog(message: "Please enter phone number", callback: validationCallBack)
+            return
+        }
+        
+        if(phoneNumber!.count > 10){
+            showAlertDialog(message: "Phone number can't be greater than 10", callback: validationCallBack)
+            return
+        }
+        
+        
+        //Hit Api
+    }
+    
+    
+    private func showAlertDialog(message: String, callback: @escaping () -> Void){
+        let alertController = UIAlertController(title: "Validation Error", message: message, preferredStyle: .alert)
+        
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default,
+        handler: { action in
+            callback()
+        }))
+        
+        self.present(alertController, animated: true)
+    }
 }
 
